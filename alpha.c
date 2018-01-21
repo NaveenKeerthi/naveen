456dc2f49808a6339a68e633e212db4158b02c48
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-01-21 16:12:18 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the letter",ch);
+scanf("%c",&ch);
+}
+if((ch>65 && ch<=90)||(ch>=92 && ch<=122))
+printf("%c is a alphet",ch);
+else
+printf("%c is a not alphet",ch);
+return 0;
+}
