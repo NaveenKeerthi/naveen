dbe98b2b6b580e2c31f01fe604e3f41681b4243e
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-09 16:17:31 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("\n Enter the any lowecase letter");
+scanf("%c",&a);
+  if((a=a)||(a=e)||(a=i)||(a=o)||(a=u))
+  {
+    printf("\n The letter is vowel");
+  }
+  else
+    printf("\n The letter is not vowel");
+  getch();
+}
