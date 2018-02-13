08f9fa5abf1fc2ce56f7c9cfe05bc06e26b63328
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-13 16:34:25 +0530

    Create void.c

---
+++ void.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int a,b,i;
+clrscr();
+scanf("%d%d",&a,&b);
+for(i=a;i<=b;i++)
+{
+if(i%2!=0)
+{
+printf("%d",i);
+}
+else
+{
+printf("\n");
+}
+}
+return 0;
+getch();
+}
