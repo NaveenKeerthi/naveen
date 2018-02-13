03c0803911579afe2e86049c734be95f32397b38
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-13 16:28:47 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h.h>
+#include<conio.h>
+int main()
+{
+int t,n,s=0,i;
+cout<<"enter the num for palindrome";
+cin>>n;
+t=n;
+while(s!=0)
+{
+t=t%10;
+  s=s*10+t;
+  t=t/10;
+}
+  if(s==n)
+    cout<<"the num is palindrome";
+  else
+    cout<<"the num is not palindrome";
+}
