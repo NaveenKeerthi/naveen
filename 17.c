4332b970bf4bd53d7accef31245fd2976da4dc16
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-13 16:42:06 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,25 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,i,x,p=1,s=0;
+clrscr();
+cout<<"enter the number";
+cin>>n;
+x=n;
+if(x!=0)
+{
+x=x/10;
+for(i=0;i<3;i++)
+{
+p=p*x;
+}
+s=p+s;
+}
+if(s==n)
+cout<<"the num is armstrong num";
+else
+cout<<"the num is not armstrong num ;"
+return 0;
+getch();
+}
