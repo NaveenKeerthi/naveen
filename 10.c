59d7e0ec96c96817dfc3f6599ae9686afe19b419
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-20 15:55:47 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,d;
+clrscr();
+cout<<"enter the num";
+cin>>n;
+d=sizeof(n)/2;
+cout<<"the num of digits is%d"<<d;
+return 0;
+getch();
+}
