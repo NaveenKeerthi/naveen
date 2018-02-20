cc877af2d46ae73c4c78fdbb6336dfee549814c9
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-20 15:42:14 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,s=1;
+clrscr();
+cout<<"enter the num ";
+cin>>n;
+for(i=1;i<=n;i++)
+{
+s=s*i;
+}
+cout<<"the factorial is %d"<<s;
+return 0;
+getch();
+}
