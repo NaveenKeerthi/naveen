39dbffeccbdfd8d10bd21bcc5284d8fbd22bdf04
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-22 15:55:17 +0530

    Rename 22.c to size of array.c

---
+++ size of array.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[100],i,t,n;
+clrscr();
+cout<<"enter the size of array";
+cin>>n;
+cout<<"enter the values";
+for(i=0;i<n;i++)
+{
+cin>>a[i];
+}
+for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+if(a[i])>a[j])
+   { 
+    t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+    }
+}
+cout<<"maximum num is %d"<<a[n-1];
+return 0;
+getch();
+}
