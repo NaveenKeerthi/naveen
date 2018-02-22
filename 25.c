2278fca806584852c63fdabb7d4bb742a3864d5e
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-22 16:17:43 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,18 @@
+include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,s,a[100];
+clrscr();
+cout<<"enter the size if array";
+cin>>n;
+  cout<<"enter the array num";
+  for(i=1;i<=n;i++)
+{
+cin>>a[i];
+}
+s=(n+1)/2;
+cout<<"the median value in array is %d"<<a[s];
+return 0;
+getch();
+}
