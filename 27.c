a56119b8b7e6bf67a2de66dc536c11c9338d5468
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-02-22 16:43:12 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char n;
+clrscr();
+cout<<"enter the string";
+cin>>n;
+if(isdigit(n)==0)
+cout<<"this is string";
+else
+cout<<"is a numeric";
+return 0;
+getch();
+}
