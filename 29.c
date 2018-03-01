18062aeb118077b59f84fa70d4418bbf7eed355a
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-03-01 15:42:03 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,13 @@
+include<stdio.h>
+#include<conio.h>
+int main()
+{
+int min,hr;
+clrscr();
+cout<<"enter the minute value";
+cin>>min;
+hr=min*0.0166667;
+cout<<"minutes in hours is %d"<<hr;
+return 0;
+getch();
+}
