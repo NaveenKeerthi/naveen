91066b5a3e1e58b723f0239e0076af10ae11419f
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-03-02 14:13:13 +0530

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,16 @@
+include<iostream.h>
+#include<conio.h>
+int main()
+{
+int i,j=0;
+char a[100];
+clrscr();
+cout<<"enter the siring";
+cin>>a[100];
+for(i=0;a[i]=='\n';i++])
+ {
+ ++j;
+ }
+ cout<<"no.of spaces %d"<<j;
+getch();
+}
