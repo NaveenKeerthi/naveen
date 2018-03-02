3d4bb122d48823bb39e0ac99a0629904ed89ec17
NaveenKeerthi <35652475+NaveenKeerthi@users.noreply.github.com>
2018-03-02 14:10:12 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,j=0;
+char a[100];
+clrscr();
+cout<<"enter the siring";
+cin>>a[100];
+for(i=0;a[i]=='\n';i++])
+ {
+ ++j;
+ }
+cout<<"no.of words %d"<<j;
+getch();
+}
